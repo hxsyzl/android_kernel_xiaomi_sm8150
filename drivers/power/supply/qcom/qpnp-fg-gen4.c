88c8ae64551b3d5c7dbbfe242c9f422b01afb590
Danny Lin <danny@kdrag0n.dev>
2019-08-24 17:09:50 -0700

    power: qpnp-fg-gen4: Expose maximum raw capacity value
    Inform userspace of the current min-max CAPACITY_RAW scale so that it
    can interpret the value correctly.
    
    Possible values:
        - 255: legacy low-resolution MSOC
        - 10000: scaled high-resolution MSOC (or scaled low-resolution BSOC
          if running on battery without my recent driver changes)
        - 65535: raw high-resolution MSOC
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    [cleaned up unused rc and fg variables]
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/power/supply/qcom/qpnp-fg-gen4.c
+++ drivers/power/supply/qcom/qpnp-fg-gen4.c
@@ -1043,6 +1043,16 @@ static int fg_gen4_get_prop_capacity_raw(struct fg_gen4_chip *chip, int *val)
 	return 0;
 }
 
+static int fg_gen4_get_prop_capacity_raw_max(struct fg_gen4_chip *chip, int *val)
+{
+	if (chip->dt.soc_hi_res)
+		*val = 10000;
+	else
+		*val = FULL_SOC_RAW;
+
+	return 0;
+}
+
 #if defined(CONFIG_MACH_XIAOMI_VAYU) || defined(CONFIG_MACH_XIAOMI_NABU)
 static int fg_gen4_get_prop_soc_decimal_rate(struct fg_gen4_chip *chip, int *val)
 {
@@ -5172,6 +5182,9 @@ static int fg_psy_get_property(struct power_supply *psy,
 		rc = fg_gen4_get_prop_soc_decimal_rate(chip, &pval->intval);
 		break;
 #endif
+	case POWER_SUPPLY_PROP_CAPACITY_RAW_MAX:
+		rc = fg_gen4_get_prop_capacity_raw_max(chip, &pval->intval);
+		break;
 	case POWER_SUPPLY_PROP_CC_SOC:
 		rc = fg_get_sram_prop(&chip->fg, FG_SRAM_CC_SOC, &val);
 		if (rc < 0) {
@@ -5549,6 +5562,7 @@ static enum power_supply_property fg_psy_props[] = {
 	POWER_SUPPLY_PROP_SOC_DECIMAL_RATE,
 #endif
 	POWER_SUPPLY_PROP_CAPACITY_RAW,
+	POWER_SUPPLY_PROP_CAPACITY_RAW_MAX,
 	POWER_SUPPLY_PROP_CC_SOC,
 	POWER_SUPPLY_PROP_TEMP,
 	POWER_SUPPLY_PROP_VOLTAGE_NOW,
